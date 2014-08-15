2f88e8908143f21a075365ecfc4a4afeeb431592
Daniel Pradilla <info@danielpradilla.info>
2014-08-15 17:08:26 +0200

    ads

--- index.php
+++ index.php
@@ -16,11 +16,24 @@
 		    </div>
 	    </div>
     </div>
+    <div class="ad">
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <ins class="adsbygoogle"
+               style="display:inline-block;width:728px;height:90px"
+               data-ad-client="ca-pub-9174635424947693"
+               data-ad-slot="1244871139"></ins>
+          <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
+    </div>
     <div id="results" class="container">
 	    		<?php include('results.php') ?>
     </div>
 
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <script src="js/functions.js"></script>
+    <?php if (file_exists('footer.php')) {
+      include('footer.php');
+    }?>
   </body>
 </html>
